9c338decc661fef241585dd5d21cdc5639133f96
SweetOps <vladimir.fess@gmail.com>
2017-10-25 17:43:35 +0300

    rework

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "peer_owner_id" {
   description = "The AWS account ID of the owner of the peer VPC"
+  default     = ""
 }
 
 variable "vpc_id" {
@@ -10,20 +11,37 @@ variable "vpc_peer_id" {
   default = "The ID of the VPC with which you are creating the VPC Peering Connection"
 }
 
+variable "same_account" {
+  default = "true"
+}
+
+variable "auto_accept" {
+  default = "true"
+}
+
 variable "requester" {
   description = ""
-  type        = "map"
+  type        = "list"
 
-  default = {
-    allow_remote_vpc_dns_resolution = true
-  }
+  default = [
+    {
+      allow_remote_vpc_dns_resolution = true
+    },
+  ]
 }
 
 variable "accepter" {
   description = ""
-  type        = "map"
+  type        = "list"
 
-  default = {
-    allow_remote_vpc_dns_resolution = true
-  }
+  default = [
+    {
+      allow_remote_vpc_dns_resolution = true
+    },
+  ]
+}
+
+variable "route_tables" {
+  type    = "list"
+  default = []
 }
