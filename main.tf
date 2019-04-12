40cab5e5ff4a2215d93c591c0210860fe5057f3c
Igor Rodionov <goruha@gmail.com>
2019-04-12 19:18:51 +0600

    Added tags to find vpc

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "label" {
 
 resource "aws_vpc_peering_connection" "default" {
   count       = "${var.enabled == "true" ? 1 : 0}"
-  vpc_id      = "${var.requestor_vpc_id}"
-  peer_vpc_id = "${var.acceptor_vpc_id}"
+  vpc_id      = "${data.aws_vpc.requestor.id}"
+  peer_vpc_id = "${data.aws_vpc.acceptor.id}"
 
   auto_accept = "${var.auto_accept}"
 
