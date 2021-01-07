c5ef3c078d7e728e3a0717966d888cde2279d7b8
Maxim Mironenko <maxim@cloudposse.com>
2021-01-07 15:19:45 +0700

    Terraform 0.14 upgrade (#28)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,62 +3,53 @@ provider "aws" {
 }
 
 module "requestor_vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.1"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  attributes = concat(var.attributes, ["requestor"])
+  source     = "cloudposse/vpc/aws"
+  version    = "0.18.1"
+  attributes = ["requestor"]
   cidr_block = var.requestor_vpc_cidr
-  tags       = var.tags
+
+  context = module.this.context
 }
 
 module "requestor_subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.18.1"
+  source               = "cloudposse/dynamic-subnets/aws"
+  version              = "0.33.0"
   availability_zones   = var.availability_zones
-  namespace            = var.namespace
-  stage                = var.stage
-  name                 = var.name
-  attributes           = concat(var.attributes, ["requestor"])
+  attributes           = ["requestor"]
   vpc_id               = module.requestor_vpc.vpc_id
   igw_id               = module.requestor_vpc.igw_id
   cidr_block           = module.requestor_vpc.vpc_cidr_block
   nat_gateway_enabled  = false
   nat_instance_enabled = false
-  tags                 = var.tags
+
+  context = module.this.context
 }
 
 module "acceptor_vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.1"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  attributes = concat(var.attributes, ["acceptor"])
+  source     = "cloudposse/vpc/aws"
+  version    = "0.18.1"
+  attributes = ["acceptor"]
   cidr_block = var.acceptor_vpc_cidr
-  tags       = var.tags
+
+  context = module.this.context
 }
 
 module "acceptor_subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.18.1"
+  source               = "cloudposse/dynamic-subnets/aws"
+  version              = "0.33.0"
   availability_zones   = var.availability_zones
-  namespace            = var.namespace
-  stage                = var.stage
-  name                 = var.name
-  attributes           = concat(var.attributes, ["acceptor"])
+  attributes           = ["acceptor"]
   vpc_id               = module.acceptor_vpc.vpc_id
   igw_id               = module.acceptor_vpc.igw_id
   cidr_block           = module.acceptor_vpc.vpc_cidr_block
   nat_gateway_enabled  = false
   nat_instance_enabled = false
-  tags                 = var.tags
+
+  context = module.this.context
 }
 
 module "vpc_peering" {
   source                                    = "../.."
-  namespace                                 = var.namespace
-  stage                                     = var.stage
-  name                                      = var.name
-  attributes                                = var.attributes
-  tags                                      = var.tags
   auto_accept                               = true
   requestor_allow_remote_vpc_dns_resolution = true
   acceptor_allow_remote_vpc_dns_resolution  = true
@@ -67,4 +58,6 @@ module "vpc_peering" {
   create_timeout                            = "5m"
   update_timeout                            = "5m"
   delete_timeout                            = "10m"
+
+  context = module.this.context
 }
--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  enabled    = var.enabled
-  namespace  = var.namespace
-  name       = var.name
-  stage      = var.stage
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
-}
-
 resource "aws_vpc_peering_connection" "default" {
-  count       = var.enabled ? 1 : 0
+  count       = module.this.enabled ? 1 : 0
   vpc_id      = join("", data.aws_vpc.requestor.*.id)
   peer_vpc_id = join("", data.aws_vpc.acceptor.*.id)
 
@@ -24,7 +13,7 @@ resource "aws_vpc_peering_connection" "default" {
     allow_remote_vpc_dns_resolution = var.requestor_allow_remote_vpc_dns_resolution
   }
 
-  tags = module.label.tags
+  tags = module.this.tags
 
   timeouts {
     create = var.create_timeout
@@ -35,33 +24,33 @@ resource "aws_vpc_peering_connection" "default" {
 
 # Lookup requestor VPC so that we can reference the CIDR
 data "aws_vpc" "requestor" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
   id    = var.requestor_vpc_id
   tags  = var.requestor_vpc_tags
 }
 
 # Lookup acceptor VPC so that we can reference the CIDR
 data "aws_vpc" "acceptor" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
   id    = var.acceptor_vpc_id
   tags  = var.acceptor_vpc_tags
 }
 
 data "aws_route_tables" "requestor" {
-  count  = var.enabled ? 1 : 0
+  count  = module.this.enabled ? 1 : 0
   vpc_id = join("", data.aws_vpc.requestor.*.id)
   tags   = var.requestor_route_table_tags
 }
 
 data "aws_route_tables" "acceptor" {
-  count  = var.enabled ? 1 : 0
+  count  = module.this.enabled ? 1 : 0
   vpc_id = join("", data.aws_vpc.acceptor.*.id)
   tags   = var.acceptor_route_table_tags
 }
 
 # Create routes from requestor to acceptor
 resource "aws_route" "requestor" {
-  count                     = var.enabled ? length(distinct(sort(data.aws_route_tables.requestor.0.ids))) * length(data.aws_vpc.acceptor.0.cidr_block_associations) : 0
+  count                     = module.this.enabled ? length(distinct(sort(data.aws_route_tables.requestor.0.ids))) * length(data.aws_vpc.acceptor.0.cidr_block_associations) : 0
   route_table_id            = element(distinct(sort(data.aws_route_tables.requestor.0.ids)), ceil(count.index / length(data.aws_vpc.acceptor.0.cidr_block_associations)))
   destination_cidr_block    = data.aws_vpc.acceptor.0.cidr_block_associations[count.index % length(data.aws_vpc.acceptor.0.cidr_block_associations)]["cidr_block"]
   vpc_peering_connection_id = join("", aws_vpc_peering_connection.default.*.id)
@@ -70,7 +59,7 @@ resource "aws_route" "requestor" {
 
 # Create routes from acceptor to requestor
 resource "aws_route" "acceptor" {
-  count                     = var.enabled ? length(distinct(sort(data.aws_route_tables.acceptor.0.ids))) * length(data.aws_vpc.requestor.0.cidr_block_associations) : 0
+  count                     = module.this.enabled ? length(distinct(sort(data.aws_route_tables.acceptor.0.ids))) * length(data.aws_vpc.requestor.0.cidr_block_associations) : 0
   route_table_id            = element(distinct(sort(data.aws_route_tables.acceptor.0.ids)), ceil(count.index / length(data.aws_vpc.requestor.0.cidr_block_associations)))
   destination_cidr_block    = data.aws_vpc.requestor.0.cidr_block_associations[count.index % length(data.aws_vpc.requestor.0.cidr_block_associations)]["cidr_block"]
   vpc_peering_connection_id = join("", aws_vpc_peering_connection.default.*.id)
